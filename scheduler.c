6dfe2706b6db29657dc80af142421e44bf0dd16d
mtheggi <mtheggi@gmail.com>
2023-12-04 18:22:20 +0200

    scheduler algos intiate

--- scheduler.c
+++ scheduler.c
@@ -1,16 +1,22 @@
 #include "headers.h"
 
+void HPF();  // highest priority first
+void SRTN(); // shortest remaining time next
+void RR();   // Round Robin
+
+int process_count, quantum_time;
+
 int main(int argc, char *argv[])
 {
-    // printf("%d", getClk());
-    // initClk();
+    initClk();
+    printf("schedulaer me %d \n", getClk());
     // argv[0] count of process ;
     // argv[1] chosen algorithm ;
     // argv[2] quantum time if RR ;
-    int process_count = atoi(argv[0]);
-    int algorithm_num = atoi(argv[1]);
-    int quantum_time = 0;
 
+    process_count = atoi(argv[0]);
+    int algorithm_num = atoi(argv[1]);
+    quantum_time = 0;
     if (algorithm_num == 3)
         quantum_time = atoi(argv[2]);
 
@@ -18,7 +24,17 @@ int main(int argc, char *argv[])
     printf("%d %d %d \n", process_count, algorithm_num, quantum_time);
 
     // TODO implement the scheduler :)
-    // upon termination release the clock resources.
+    if (algorithm_num == 1)
+        HPF();
+    else if (algorithm_num == 2)
+        SRTN();
+    else if (algorithm_num == 3)
+        RR();
 
+    // upon termination release the clock resources.
     // destroyClk(true);
+}
+
+void RR()
+{
 }
