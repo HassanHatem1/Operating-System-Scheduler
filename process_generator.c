f65d139c2eb51e83cc24b763f503ddaea54e20b2
MalekElsaka <102513380+MalekElsaka@users.noreply.github.com>
2023-12-04 14:04:15 +0200

    Added msg_queue (P_Generator)

--- process_generator.c
+++ process_generator.c
@@ -1,140 +1,165 @@
 #include "headers.h"
-#include <stdio.h>
-
-typedef struct
-{
-    int id;
-    int priority;
-    int arrival_time;
-    int running_time;
-} Process;
 
 void clearResources(int);
 
-int main(int argc, char *argv[])
+int msgq_id, shmid;
+
+void arrivalsSharedMemory()
+{
+    key_t shmKey;
+    shmKey=ftok("keyfile",120);
+    shmid=shmget(shmKey, sizeof(int) *1000, IPC_CREAT|0666);
+    if (shmid == -1)
+    {
+        perror("Error in creating the shared memory");
+        exit(-1);
+    }
+}
+
+int main(int argc, char * argv[])
 {
     signal(SIGINT, clearResources);
 
-    // 1. Read the input files.
-    FILE *file = fopen("processes.txt", "r");
-    if (file == NULL)
+    // Initialization
+
+    arrivalsSharedMemory();
+    int *arrivals;
+    arrivals = (int*) shmat(shmid, 0, 0);
+    for(int i = 0;i < 1000; i++)
+        arrivals[i] = 0;
+
+    key_t key_id;
+    int send_val;
+    FILE *key = fopen("keyfile", "r");
+    key_id = ftok("keyfile", 65);
+    msgq_id = msgget(key_id, 0666 | IPC_CREAT);
+    if (msgq_id == -1)
     {
-        printf("Error! File not found.\n");
+        perror("Error in creating the message queue\n");
+        exit(-1);
+    }
+    else
+        printf("Message Queue created successfully!\n");
+
+    // ------ 1. Read the input files.
+
+    FILE *file = fopen("processes.txt", "r"); // Opening the File to get the number of processes
+    if (file == NULL) {
+        printf("Error! File not opened.\n");
+        return 1;
+    }
+    int numberOfProcesses = 0;
+    fscanf(file, "%*[^\n]");
+    while (!feof(file)) {
+        int i, a, r, p;
+        fscanf(file, "%d\t%d\t%d\t%d", &i, &a, &r, &p);
+        numberOfProcesses = i;
+    }
+    printf("Number of Processes: %d\n", numberOfProcesses);
+
+    fclose(file);
+    file = fopen("processes.txt", "r"); // Opening again to store the processes
+    if (file == NULL) {
+        printf("Error! File not opened.\n");
         return 1;
     }
 
-    // get the number of processes from the document by counting the lines
-    int processes_count = 0;
-    char ch;
-    while (!feof(file))
-    {
-        ch = fgetc(file);
-        if (ch == '\n')
-            processes_count++;
+    struct processData *processes = malloc(sizeof(struct processData) * numberOfProcesses); // Array of processes
+    fscanf(file, "%*[^\n]");
+    int index = 0;
+    printf("***Input***\n");
+    printf("id   arrival   runtime   priority\n");
+    while (index < numberOfProcesses) {
+        int i, a, r, p;
+        fscanf(file, "%d\t%d\t%d\t%d", &i, &a, &r, &p);
+        processes[index].id = i;
+        processes[index].arrivaltime = a;
+        processes[index].runningtime = r;
+        processes[index].priority = p;
+        arrivals[a]++;
+        printf("%d\t%d\t%d\t%d\n", i, a, r, p);
+        index++;
     }
-    fclose(file);
-
-    // create an array of processes with the size of the number of processes
-    Process processes[processes_count];
-
-    // reopen the file to read the processes data from the document and store them in the array
-    file = fopen("processes.txt", "r");
-    for (int i = 0; i < processes_count; i++)
-        // here t%d will make fscanf ignore the space and read the number only
-        fscanf(file, "%d t%d t%d t%d", &processes[i].id, &processes[i].priority, &processes[i].arrival_time, &processes[i].running_time);
-    fclose(file);
+    fclose(file); // CLose File
 
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
-    int algorithm_num;
-    printf("-----------------PROCESS_GENERATOR-----------------\n");
-    printf("Choose a scheduling algorithm from the following:\n");
+    
+    int algorithm;
+    printf("-------------------------------\n");
+    printf("Choose a scheduling algorithm:\n");
     printf("1. Non-preemptive Highest Priority First (HPF).\n");
     printf("2. Shortest Remaining time Next (SRTN).\n");
     printf("3. Round Robin (RR).\n");
-    scanf("%d", &algorithm_num);
-    while(algorithm_num < 1 || algorithm_num > 3)
-    {
-        printf("Please enter a valid algorithm number: ");
-        scanf("%d", &algorithm_num);
-    }
-    printf("---------------------------------------------------\n");
+    scanf("%d", &algorithm);
+    printf("-------------------------------\n");
 
-    int quantum;
-    if (algorithm_num == 3)
-    {
-        printf("Enter the quantum for the Round Robin algorithm: ");
-        scanf("%d", &quantum);
+    int timeSlot;
+    if (algorithm == 3) {
+        printf("Enter the time slot for Round Robin: ");
+        scanf("%d", &timeSlot);
     }
 
     // 3. Initiate and create the scheduler and clock processes.
 
-    // Create the scheduler process
-    pid_t scheduler_pid = fork();
-    if (scheduler_pid == -1)
+    // Scheduler
+    printf("Forking Scheduler..\n");
+    int pid = fork();
+    if (pid == -1) 
         perror("Error in creating scheduler process\n");
-    else if (scheduler_pid == 0) // This is the scheduler process
-    {
-        // compile the C program named scheduler.c using the gcc compiler with certain flags ().
-        // The compiled output is named scheduler.out.
+    else if (pid == 0) {
         system("gcc -Wall -o scheduler.out scheduler.c -lm -fno-stack-protector");
         printf("Scheduling..\n");
-
-        // execute the scheduler.out file with the arguments of the number of processes and the algorithm number (and the quantum if it's RR)
-        // sprintf is used to convert the integer arguments to strings to be passed to the execl function
-        char process_count_str[10], algorithm_num_str[2], quantum_str[10];
-        sprintf(process_count_str, "%d", processes_count);
-        sprintf(algorithm_num_str, "%d", algorithm_num);
-
-        // here the NULL in the execl function serves as a sentinel value to indicate the end of the argument list.
-        if (algorithm_num != 3)
-            execl("./scheduler.out", process_count_str, algorithm_num_str, NULL);
+        char n_str[10], a_str[10], t_str[10];
+        if (algorithm == 3)
+            execl("./scheduler.out", string(numberOfProcesses, n_str), string(algorithm, a_str), string(timeSlot, t_str), NULL);
         else
-        {
-            sprintf(quantum_str, "%d", quantum);
-            execl("./scheduler.out", process_count_str, algorithm_num_str, quantum_str, NULL);
+            execl("./scheduler.out",  string(numberOfProcesses, n_str), string(algorithm, a_str), NULL);
+    }
+
+    // Clock
+    if (pid != 0) {
+        printf("Forking Clock..\n");
+        int clkpid = fork();
+        if (clkpid == -1) 
+            perror("Error in creating clock process\n");
+        else if (clkpid == 0) {
+            system("gcc clk.c -o clk.out -fno-stack-protector");
+            execl("./clk.out", "clk", NULL);
         }
-        exit(0);
-    }
+        else {
+            // 4. Use this function after creating the clock process to initialize clock
 
-    // Create the clock process
-    pid_t clock_pid = fork();
-    if (scheduler_pid == -1)
-        perror("Error in creating scheduler process\n");
-    else if (clock_pid == 0) // This is the clock process
-    {
-        // compile the C program named clk.c using the gcc compiler with certain flags().
-        // The compiled output is named clk.out.
-        system("gcc clk.c -o clk.out -fno-stack-protector");
-        execl("./clk.out", NULL); // execute the clk.out file
-        exit(0);
-    }
+            initClk();
 
-    // 4. Use this function after creating the clock process to initialize clock
-    initClk();
+            // TODO Generation Main Loop
+            // 5. Create a data structure for processes and provide it with its parameters.
 
-    // 5. Create a data structure for processes and provide it with its parameters.
-    // --> already done in the beginning of the main function
+                // --> struct processData *processes = malloc(sizeof(struct processData) * numberOfProcesses);
 
-    // 6. Send the information to the scheduler at the appropriate time.
-    int index = 1;
-    while (index <= processes_count)
-    {
-        if (processes[index].arrival_time == getClk())
-        {
-           // send_val = msgsnd(msgq_id, &processes[current], sizeof(struct processData), !IPC_NOWAIT);
-            //current++;
+            // 6. Send the information to the scheduler at the appropriate time.
+
+            int current = 0;
+            //printf("-------------------------------\n");
+            while (current < numberOfProcesses) {
+                int arrival = processes[current].arrivaltime;
+                if (arrival == getClk()) {
+                    //printf("Sending Process %d\n", current + 1);
+                    //printf("Current Time: %d, Arrival: %d\n", getClk(), arrival);
+                    send_val = msgsnd(msgq_id, &processes[current], sizeof(struct processData), !IPC_NOWAIT); //
+                    current++;
+                }
+            }
+            // 7. Clear clock resources
+            int stat_loc;
+            waitpid(pid, &stat_loc, 0);
         }
     }
-
-// 7. Clear clock resources
-destroyClk(true);
 }
-
 
 void clearResources(int signum)
 {
-    // TODO Clears all resources in case of interruption
-    printf("Clearing due to interruption\n");
-    // msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
+    //TODO Clears all resources in case of interruption
+    printf("Clearing\n");
+    msgctl(msgq_id, IPC_RMID, (struct msqid_ds *)0);
     raise(SIGKILL);
 }
