0229d08f416d401d1655fc40079d859eaf0ed07b
mtheggi <mtheggi@gmail.com>
2023-12-05 19:39:13 +0200

    Queue and Priority Queue Tested and added

--- headers.h
+++ headers.h
@@ -66,7 +66,7 @@ void destroyClk(bool terminateAll)
     }
 }
 // ---------------------------process contorl block structure --------------------------------
-typedef struct
+struct PCB
 {
     int id;
     int pid;
@@ -78,9 +78,9 @@ typedef struct
     int priority;
     int start;
     int wait;
-} PCB;
+};
 
-void setPCB(PCB *pcb, int nID, int nPID, int nArrival, int nBurst, int nFinish, int nRunning, int nStop, int nPriority, int nStart, int nWait)
+void setPCB(struct PCB *pcb, int nID, int nPID, int nArrival, int nBurst, int nFinish, int nRunning, int nStop, int nPriority, int nStart, int nWait)
 {
     pcb->id = nID;
     pcb->pid = nPID;
@@ -100,85 +100,174 @@ struct Queue
 {
     int front, rear, size;
     unsigned capacity;
-    PCB *array;
+    struct PCB **array; // array of pointers
 };
 
 // function to create a queue of given capacity.
 // It initializes size of queue as 0
-struct Queue * createQueue(unsigned capacity)
+struct Queue *createQueue(unsigned capacity)
 {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    struct Queue *queue = (struct Queue *)malloc(sizeof(struct Queue));
     queue->capacity = capacity;
     queue->front = queue->size = 0;
- 
+
     queue->rear = capacity - 1;
-    queue->array = (PCB*)malloc(queue->capacity * sizeof(PCB));
+    queue->array = (struct PCB **)malloc(queue->capacity * sizeof(struct PCB *));
     return queue;
 }
 
 // Queue is full when size becomes
 // equal to the capacity
-int isFull(struct Queue* queue)
+int isFull(struct Queue *queue)
 {
     return (queue->size == queue->capacity);
 }
- 
+
 // Queue is empty when size is 0
-int isEmpty(struct Queue* queue)
+int isEmpty(struct Queue *queue)
 {
     return (queue->size == 0);
 }
 
 // Function to add an item to the queue.
 // It changes rear and size
-void enqueue(struct Queue* queue, PCB item)
+void enqueue(struct Queue *queue, struct PCB *process)
 {
     if (isFull(queue))
         return;
     queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->array[queue->rear] = item;
+    queue->array[queue->rear] = process;
     queue->size = queue->size + 1;
-    // printf("%d enqueued to queue\n", item);
 }
 
 // Function to remove an item from queue.
 // It changes front and size
-PCB dequeue(struct Queue * queue)
+struct PCB *dequeue(struct Queue *queue)
 {
     if (isEmpty(queue))
     {
-        PCB pcb;
-        setPCB(&pcb, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
-        return pcb;
-        
+        return NULL;
     }
-    PCB item = queue->array[queue->front];
+    struct PCB *process = queue->array[queue->front];
     queue->front = (queue->front + 1) % queue->capacity;
     queue->size = queue->size - 1;
-    return item;
+    return process;
 }
-PCB front(struct Queue* queue)
+struct PCB *front(struct Queue *queue)
 {
     if (isEmpty(queue))
     {
-        PCB pcb;
-        setPCB(&pcb, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
-        return pcb;
-        
+        return NULL;
     }
     return queue->array[queue->front];
 }
- 
+
 // Function to get rear of queue
-PCB rear(struct Queue* queue)
+struct PCB *rear(struct Queue *queue)
 {
     if (isEmpty(queue))
     {
-        PCB pcb;
-        setPCB(&pcb, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
-        return pcb;
-        
+        return NULL;
     }
     return queue->array[queue->rear];
 }
+void QueuePrint(struct Queue *queue)
+{
+    int index = 1;
+    for (int i = queue->front; i <= queue->rear; i++)
+    {
+        printf("the node number %d which has id %d and burst time %d\n", index, queue->array[i]->id, queue->array[i]->brust);
+        index++;
+    }
+}
+// --------------------Priority Queue implementation for HPF /STRN => src "Geeks for Geeks" ----------------------------
+typedef struct node
+{
+    struct PCB *process;
 
+    // Lower values indicate higher priority
+    int priority;
+
+    struct node *next;
+
+} Node;
+
+// Function to Create A New Node
+Node *newNode(struct PCB *d, int p)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->process = d;
+    temp->priority = p;
+    temp->next = NULL;
+
+    return temp;
+}
+
+// Return the value at head
+struct PCB *peek(Node **head)
+{
+    return (*head)->process;
+}
+
+// Removes the element with the
+// highest priority from the list
+void pop(Node **head)
+{
+    Node *temp = *head;
+    (*head) = (*head)->next;
+    free(temp);
+}
+
+// Function to push according to priority
+void push(Node **head, struct PCB *d, int p)
+{
+    Node *start = (*head);
+
+    // Create new Node
+    Node *temp = newNode(d, p);
+
+    // Special Case: The head of list has lesser
+    // priority than new node. So insert new
+    // node before head node and change head node.
+    if ((*head)->priority > p)
+    {
+
+        // Insert New Node before head
+        temp->next = *head;
+        (*head) = temp;
+    }
+    else
+    {
+
+        // Traverse the list and find a
+        // position to insert new node
+        while (start->next != NULL &&
+               start->next->priority < p)
+        {
+            start = start->next;
+        }
+
+        // Either at the ends of the list
+        // or at required position
+        temp->next = start->next;
+        start->next = temp;
+    }
+}
+
+// Function to check is list is empty
+int isEmptyPQ(Node **head)
+{
+    return ((*head) == NULL);
+}
+
+void PQueuePrint(Node **head)
+{
+    Node *start = *head;
+    int index = 1;
+    while (start)
+    {
+        printf("the node number %d which has id %d and burst time %d\n  and priority %d \n", index, start->process->id, start->process->brust, start->priority);
+        index++;
+        start = start->next;
+    }
+}
