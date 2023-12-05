5e5171d40dd539cef271719865be8f3790b5d7d5
HassanHatem1 <hassan.hatem1282@gmail.com>
2023-12-05 22:38:07 +0200

    work to do on while loop condition

--- process.c
+++ process.c
@@ -1,20 +1,30 @@
 #include "headers.h"
-
 /* Modify this file as needed*/
 int remainingtime;
-
-int main(int agrc, char * argv[])
+int main(int agrc, char *argv[])
 {
     initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
+    key_t key = ftok("keyfile", 44); // for shared memory
+    int id = atoi(argv[0]); // id of process (to send it in the notification to the scheduler)
+    int Arr_Size=atoi(argv[1])+1; //+1 because if you have 20 processess id from 1 to 20 not 0 to 19 so it must be size +1
+    int shmid = shmget(key,Arr_Size*sizeof(int), IPC_CREAT | 0666);
+    int *sharedMem = shmat(shmid, (void *)0, 0);
+    if (sharedMem == (void *)-1)
     {
-        // remainingtime = ??;
+        perror("Error in attach ");
+        exit(-1);
     }
-    
+    int prevClk = getClk();
+    remainingtime=sharedMem[id];
+    while (remainingtime > 0) ////////////work to do here
+    {
+        if (prevClk != getClk())
+        {
+            remainingtime--; //no need for this variable
+            sharedMem[id]--;
+        }
+        prevClk = getClk();
+    }
     destroyClk(false);
-    
     return 0;
 }
